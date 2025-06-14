606dc6d837be4958fccc7aebd3e08242697a68ff
dalisoto <daliadelcarmenmendiolasoto@gmail.com>
2025-06-13 20:29:03 -0600

    Prueba

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Dalia del Carmen Mendiola Soto 
-          
+          Dalia del Carmen Mendiola Soto GIDS5093
         </p>
         <a
           className="App-link"
